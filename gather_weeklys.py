72cfe1a45059b602649186f634fcd8ffca9b59d3
gazzman <sgazzisg@uw>
2013-06-11 07:31:21 -0400

    Debugging.

--- gather_weeklys.py
+++ gather_weeklys.py
@@ -4,16 +4,17 @@ __author__ = "gazzman"
 __copyright__ = "(C) 2013 gazzman GNU GPL 3."
 __contributors__ = []
 import argparse
+import sys
 
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy import Column, Date, String
 from sqlalchemy.schema import CreateSchema
 from sqlalchemy.exc import IntegrityError, ProgrammingError
 from sqlalchemy.dialects.postgresql import VARCHAR
+import xlrd
 
 STARTPATTERN = 'LIST OF AVAILABLE WEEKLYS OPTIONS'
 HEADERPATTERN = 'Ticker Symbol'
-
 EXPCOLHEADERS = ['expiry_0', 'expiry_1', 'expiry_2',
                  'expiry_3', 'expiry_4', 'expiry_5']
 
@@ -32,18 +33,12 @@ def gen_table(tablename, metadata, schema=None):
                   schema=schema)
     return table
 
-def bar_to_db(conn, table, symbol, timestamp, bar):
-    try:
-        conn.execute(table.insert(), ticker=symbol, timestamp=timestamp, **bar)
-    except IntegrityError as err:
-        if 'duplicate key' in str(err): pass
-        else: raise(err)
-
 if __name__ == "__main__":
-    description = 'A utility for storing the CBOE\'s weeklies in a database.'
-    filename_help = 'the filename of the xls spreasheet of available weeklies'
+    description = 'A utility for storing the CBOE\'s weeklys in a database.'
+    filename_help = 'the filename of the xls spreasheet of available weeklys'
     db_help = 'the name of a postgresql database'
-    tbl_help = 'the name of the table in which to store the data'
+    tbl_help = 'the name of the table in which to store the data.'
+    tbl_help += ' defaults to \'available_weeklys\''
     schema_help = 'an optional database schema'
     host_help = 'the host on which the db lives'
     
@@ -52,7 +47,7 @@ if __name__ == "__main__":
     p.add_argument('database', help=db_help)
     p.add_argument('--host', default='', help=host_help)
     p.add_argument('--schema', help=schema_help)
-    p.add_argument('--tablename', default='available_weeklies', help=tbl_help)
+    p.add_argument('--tablename', default='available_weeklys', help=tbl_help)
     p.add_argument('-v', '--version', action='version', 
                    version='%(prog)s ' + __version__)
     args = p.parse_args()
@@ -71,22 +66,22 @@ if __name__ == "__main__":
     table = gen_table(args.tablename, metadata, schema=args.schema)
     metadata.create_all()
     print >> sys.stderr, "Preparing to write to table %s.%s" % (args.schema,
-                                                                tablename)
+                                                                args.tablename)
 
-    # Parse the xls table into a list
-    wb = xlrd.open_workbook(args.file)
+    # Parse the xls data into a list
+    wb = xlrd.open_workbook(args.filename)
     sh = wb.sheet_by_index(0)
-    table = [sh.row_values(x) for x in xrange(sh.nrows)]
-    starts = [x for x in table if STARTPATTERN in x[0]]
+    data = [sh.row_values(x) for x in xrange(sh.nrows)]
+    starts = [x for x in data if STARTPATTERN in x[0]]
     if len(starts) < 1: raise Exception('No list detected with %s' 
                                         % STARTPATTERN)
 
     # Separate the table by week    
-    sidxs = [table.index(x) for x in starts]
+    sidxs = [data.index(x) for x in starts]
     weeks = []
     while len(sidxs) > 0:
-        weeks.append(table[sidxs[-1]:])
-        table = table[0:sidxs[-1]]
+        weeks.append(data[sidxs[-1]:])
+        data = data[0:sidxs[-1]]
         sidxs = sidxs[:-1]
 
     # For each week, write the data to the database
@@ -95,20 +90,22 @@ if __name__ == "__main__":
         if len(header) < 1: raise Exception('No header detected with %s'
                                             % HEADERPATTERN)
         start = week.index(header)
-        datarows = [x for x in week[start:] if x[0].strip() != '']
+        datarows = [x for x in week[start+1:] if x[0].strip() != '']
         expiry_dates = [str(x).split('.')[0] for x in header[-6:]]
         for row in datarows:
             expirys = row[-6:]
             for i in xrange(len(expirys)):
-                if expirys[i].strip() == '': expiry[i] = None
-                else: expirys[i] == expiry_dates[i]
+                if expirys[i].strip() == '': expirys[i] = None
+                else: expirys[i] = expiry_dates[i]
             expdata = dict(zip(EXPCOLHEADERS, expirys))
+            ld = str(row[3]).split('.')[0]
             try:
-                conn.execute(table.insert(), ticker=row[0], 
-                                                name=row[1],
-                                                type=row[2],
-                                                list_date=row[3],
-                                                **expdata)
+                print >> sys.stderr, "Writing %s for %s" % (row[0], ld)
+                conn.execute(table.insert(), ticker=row[0].replace('*',''),
+                                             name=row[1].replace('*',''),
+                                             type=row[2].replace('*',''),
+                                             list_date=ld,
+                                             **expdata)
             except IntegrityError as err:
                 if 'duplicate key' in str(err): pass
                 else: raise(err)
